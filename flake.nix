5455679504e49c55c0e40889fed97e97bf600572
peigongdsd <peigong2013@outlook.com>
2026-03-01 14:45:58 +0000

    Add full GStreamer plugin sets to devshell

--- flake.nix
+++ flake.nix
@@ -21,6 +21,9 @@
             glib
             gst_all_1.gstreamer
             gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
           ];
 
           shellHook = ''
